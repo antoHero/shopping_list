9b0f9eb322a871cffff4f4d7590611032ac83f16
antoHero <veeqanto@gmail.com>
2020-01-27 23:49:55 +0100

    deployed to heroku. optimize

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://antoAkay:F2EXtZxrV9E0uLZb@mernstack-n3ipk.mongodb.net/test?retryWrites=true&w=majority'
+}
---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const items = require('./routes/api/items');
+
+const app = express();
+
+//BodyParser Middleware
+app.use(bodyParser.json());
+
+//Connect to DB
+const db = require('./config/keys').mongoURI;
+
+//Connect to MongoDB 
+mongoose
+.connect(db)
+.then(() => console.log('Successfully Connected to Mongoose'))
+.catch(err => console.log(err));
+
+//Use Routes
+app.use('/api/items', items);
+
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client' , 'build', 'index.html'));
+    });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
+
